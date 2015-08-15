16e1f275721e2dbb5a26b59aaa51f898e6e9d7b3
JT Herndon <jherndon8@gatech.edu>
2015-08-15 00:55:15 -0400

    Initial Commit Added .gitignore and Dot.java

---
+++ Dot.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+/**
+ * This is a class for a dot, or a marching member.
+ * It contains the information for the front to back and side to side
+ * values for each set.
+ * @author JT Herndon
+ * @version 1.0
+ */
+public class Dot {
+    private int set_length;
+    private ArrayList<dotEntry> dot_list;
+    private class dotEntry {
+        public double front_to_back;
+        public double side_to_side;
+        public dotEntry(double front_to_back, double side_to_side) {
+            this.front_to_back = front_to_back;
+            this.side_to_side = side_to_side;
+        }
+    }
+    public Dot() {
+        dot_list = new ArrayList<>();
+        set_length = -1;
+    }
+    public void addSet(double frontToBack, double sideToSide) {
+        set_length++;
+        dot_list.add(new dotEntry(frontToBack, sideToSide));
+    }
+    public double getFrontToBack(int set) {
+        return dot_list.get(set).front_to_back;
+    }
+    public double getSideToSide(int set) {
+        return dot_list.get(set).side_to_side;
+    }
+    public int getSetLength() {
+        return set_length;
+    }
+}
